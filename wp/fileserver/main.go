8c791295a08ec390278aa63bccea2f76a87bc8a5
Bernhard Saumweber <bernhard.saumweber@qaware.de>
2020-11-06 09:20:33 +0100

    Added static file server

---
+++ wp/fileserver/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("."))
+	http.Handle("/", fileServer)
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
