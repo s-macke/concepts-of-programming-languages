43cbfe2e01cd4203a47093c30d99a8ccec1e158c
Bernhard Saumweber <bernhard.saumweber@qaware.de>
2020-11-26 22:55:41 +0100

    Fixed slides, added playable example

---
+++ sp/samples/random/main.go
@@ -0,0 +1,19 @@
+package main
+
+// #include &lt;stdlib.h&gt;
+import "C"
+import "fmt"
+
+func Seed(i int) {
+	C.srandom(C.uint(i))
+}
+
+func Random() int {
+	var r C.long = C.random() // calls "int rand(void)" from the C std library
+	return int(r)
+}
+
+func main() {
+	Seed(1)
+	fmt.Printf("random int from C is %d", Random())
+}
