a98d3218f93f1c090ed1899b1945836750967e3d
Bernhard Saumweber <bernhard.saumweber@qaware.de>
2020-12-14 15:15:13 +0100

    Added solution for wolf/goat/cabbage problem

--- logic/river.pl
+++ logic/river.pl
@@ -9,3 +9,30 @@
 %
 % Hinweis: es sind nur 7 Züge erforderlich.
 
+% Definition der gültige Züge
+% Es ist möglich von a nach b und zurück zu wechseln.
+cross(a,b).
+cross(b,a).
+% Der Mann fährt immer mit und kann genau einen Passagier oder nichts mitnehmen).
+move([X,X,Ziege,Kohl],wolf,[Y,Y,Ziege,Kohl]) :- cross(X,Y).
+move([X,Wolf,X,Kohl],ziege,[Y,Wolf,Y,Kohl]) :- cross(X,Y).
+move([X,Wolf,Ziege,X],kohl,[Y,Wolf,Ziege,Y]) :- cross(X,Y).
+move([X,Wolf,Ziege,Kohl],nichts,[Y,Wolf,Ziege,Kohl]) :- cross(X,Y).
+
+
+% Wenn der Mann bei der Ziege oder bei Wolf und Kohl ist, ist der Zustand sicher.
+safe([Farmer,_,Ziege,_]) :- Farmer = Ziege.
+safe([Farmer,Wolf,_,Kohl]) :- Farmer = Wolf, Farmer = Kohl.
+
+% Rekursive Lösungsdefinition (ähnlich vollständiger Induktion):
+% Der Endzustand ist eine gültige Lösung, d.h. also wenn alle den Fluss
+% überquert haben und bei Seite b sind.
+solution([b,b,b,b],[]).
+% Überquerungen, die zu einem sicheren Zustand führen und einer bekannten
+% Lösung führen, sind ebenfalls gültige Lösungen.
+solution(State,[Move|OtherMoves]) :- move(State,Move,NextState),
+                                     safe(NextState),
+                                     solution(NextState,OtherMoves).
+
+run :- length(X,7), solution([a,a,a,a],X), write(X).
+% solution([a,a,a,a], [ziege,nichts,wolf,ziege,kohl,nichts,ziege]).
