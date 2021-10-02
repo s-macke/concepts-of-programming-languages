8523e2d3f0f4b103a69283a1f63ac3a622ec5777
Sebastian Macke <sebastian.macke@qaware.de>
2021-10-02 21:06:14 +0200

    Further improvements

--- docs/exercises/exercise-1/Windows/software/set-env.cmd
+++ docs/exercises/exercise-1/Windows/software/set-env.cmd
@@ -4,7 +4,6 @@ type ascii-art.txt
 
 set SEU_HOME=G:
 
-set GOROOT=%SEU_HOME%\software\goroot
 set GOPATH=%SEU_HOME%\codebase
 set GOBIN=%SEU_HOME%\codebase\bin
-set PATH=%SEU_HOME%\software\goroot\bin;%PATH%
+set PATH=%SEU_HOME%\software\go\bin;%PATH%
--- docs/exercises/exercise-1/Windows/start-console.cmd
+++ docs/exercises/exercise-1/Windows/start-console.cmd
@@ -1,5 +1,5 @@
 @echo off
-call set-env.cmd
+call software\set-env.cmd
 
 cd %SEU_HOME%\codebase\src
 cmd /k
