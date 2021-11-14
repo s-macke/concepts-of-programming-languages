57156751c8077cf061ac65a9f6c269730d4d6fdb
Sebastian Macke <sebastian.macke@qaware.de>
2021-11-14 18:44:38 +0100

    Prepare Networking and Systems lecture

--- src/servers/tcp/echo/server/echo.go
+++ src/servers/tcp/echo/server/echo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 )
 
@@ -11,18 +12,16 @@ import (
 func main() {
 	l, err := net.Listen("tcp4", ":8080")
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 	defer l.Close()
-
 	for {
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		go func() {
+		go func() { // asynchroneaous reply
 			data, _ := bufio.NewReader(c).ReadString('\n')
 			_, _ = c.Write([]byte("Echo: " + data))
 			c.Close()
