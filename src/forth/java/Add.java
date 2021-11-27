b1cbec3ac9beafaa3e6143d9b05f822d794bcfdc
Sebastian Macke <sebastian@macke.de>
2021-11-27 14:08:09 +0100

    Prepare for Forth lecture

--- src/forth/java/Add.java
+++ src/forth/java/Add.java
@@ -7,4 +7,24 @@ public class Add
     public int Add(int a, int b, int c) {
         return a + b + c;
     }
+
+    public int ifthen(int a, int b, int c) {
+        int d;
+        if (a > b) {
+            d = a + c;
+        } else {
+            d = b + c;
+        }
+        return d;
+    }
+
+    public int forloop() {
+        int a = 0;
+        for(int i=0; i<10; i++) {
+            a = a + i;
+        }
+        return a;
+    }
+
+
 }
